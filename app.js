5671c709cdea79f66c3d2a8fb2b8357e7043eb7b
Varga Mátyás <matyi789456123@gmail.com>
2018-02-16 09:30:45 +0100

    kepek eltüntetve

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-/*jslint devel: true*/
-console.log("hello world");
 
-$('.image-popup').magnificPopup({
-    type: 'image'
-});
+/*jslint devel: true */
+/*global $, jQuery, alert*/
+/*jslint indent: 2 */
+
+
+(function () {
+  'use strict';
+  console.log("hello world");
+  $('.image-popup').magnificPopup({
+    type: 'image',
+      gallery:{
+      enabled:true,
+  }
+  });
+}());
+[].forEach.call(document.querySelectorAll('.image-popup'),
+    function (el){
+    el.style.display = 'none';
+})
+// ua mint a fenti kod csak jquery vel
+$('.image-popup').css('display','none');
+/////eltüntetik a képeket
